0f0a3b097fc1e8b489aaa21ff7ad785199637a07
youtissoum <youtissoum@outlook.fr>
2022-08-23 11:09:37 +0200

    add entering the amount of ticks in debug.py

--- debug.py
+++ debug.py
@@ -7,7 +7,7 @@ before = time.time()
 cellmachine = CellMachine()
 
 # cellmachine.parse_code(input("Please enter your code : "))
-cellmachine.parse_code("V3;2P;2P;{(0(35h)a(2O(2O)S(5r(tG)6c(2N(2M)dbdddhd(2O(2I)h9JJJ$${{CaA)17(5r(2B)b(5r(2J)d$d}d$(2O(2L)hdd9(2O(2K)bbfh}26GG(aM(2y)(2O)8hjdB3(iZ(2L)}B}(83(2J){{d)04(3ZV(35i)(0(o9);;;0")
+cellmachine.parse_code(input("enter code : "))
 
 tick_amount = int(input("Enter the amount of ticks : "))
 # timed

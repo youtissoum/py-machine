f18ce6b4df87c51959ad027c9f22c771e7027b5d
youtissoum <youtissoum@outlook.fr>
2022-09-21 19:09:54 +0200

    trying to make cython work with installing

--- setup.py
+++ setup.py
@@ -1,5 +1,24 @@
-from setuptools import setup
+from ast import literal_eval
+from setuptools import Extension, setup
+import sys
+
+USE_CYTHON = False
+
 from Cython.Build import cythonize
 
+cython_name = 'Cython'
+if 'Cython' in sys.modules:
+    USE_CYTHON = True
+print(f"INSTALLED MODULES : {sys.modules}")
+
+ext = '.pyx' if USE_CYTHON else '.c'
+
+extensions = [Extension("Python_Machine.level_save", ["cython/level_save"+ext])]
+
+if USE_CYTHON:
+    print("If this causes error, try uninstall cython")
+    from Cython.Build import cythonize
+    extensions = cythonize(extensions)
+
 if __name__ == '__main__':
-    setup(ext_modules=cythonize(["src/Python_Machine/level_save.pyx"]))
+    setup(ext_modules=extensions)
